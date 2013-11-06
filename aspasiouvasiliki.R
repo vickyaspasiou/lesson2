606db0dfe5ee7c40db7e2ebf620abf942a0c3d7b
vickyaspasiou <vasilikiaspasiou@wur.nl>
2013-11-06 16:25:52 +0100

    initial repostitory

---
+++ aspasiouvasiliki.R
@@ -0,0 +1,20 @@
+rm(list = ls())
+datdir=getwd()
+library(rasta)
+
+automap=function(ctr="FR",ctrlvl=2) {
+  
+  map <- raster::getData("GADM", country = ctr,level = ctrlvl, path = datdir)
+  
+  
+  plot(map)
+  plot(map, bg = "dodgerblue", axes=T)
+  
+  plot(map, lwd = 3, border = "skyblue", add=T)
+  plot(map, col = "green4", add = T)
+  grid()
+  box()
+  
+  
+  automap() 
+   
